d43c4fd0fda2dcb2bbf3b11518ccf2be4ee43367
VikingVik123 <vodimmegwa@gmail.com>
2024-01-08 16:21:57 -0800

    Added task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+"""  returns the list of available attributes
+and methods of an object
+"""
+
 def lookup(obj):
     """Returns a list of an object's available attributes."""
     return (dir(obj))
