8b8766322b9234fb4f6cdfeea46a19ec3e723498
VikingVik123 <vodimmegwa@gmail.com>
2024-01-18 07:36:09 -0800

    Updated docs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,8 +4,25 @@ from models.base import Base
 Defines a class Rectangle
 """
 class Rectangle(Base):
+    """
+    Creates a class Rectangle
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
         """Call the super class with id"""
+
         super().__init__(id)
 
         """Assign each argument width, height, x, and y to the right attribute"""
@@ -17,6 +34,7 @@ class Rectangle(Base):
     """Public getter and setter for width"""
     @property
     def width(self):
+        """set the width"""
         return self.__width
 
     @width.setter
@@ -31,6 +49,7 @@ class Rectangle(Base):
     """Public getter and setter for height"""
     @property
     def height(self):
+        """set the height"""
         return self.__height
 
     @height.setter
@@ -44,6 +63,7 @@ class Rectangle(Base):
     """Public getter and setter for x"""
     @property
     def x(self):
+        """ set x distance"""
         return self.__x
 
     @x.setter
@@ -55,6 +75,7 @@ class Rectangle(Base):
     """Public getter and setter for y"""
     @property
     def y(self):
+        """ set the y distance """
         return self.__y
 
     @y.setter
@@ -75,6 +96,16 @@ class Rectangle(Base):
             print(" " * self.x + "#" * self.width)
     
     def update(self, *args, **kwargs):
+        """ updates values with user input
+            Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents width attribute
+                - 3rd argument represent height attribute
+                - 4th argument represents x attribute
+                - 5th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
         if args:
             if len(args) >= 1:
                 self.id = args[0]
@@ -92,6 +123,7 @@ class Rectangle(Base):
 
 
     def to_dictionary(self):
+        """returns the dict rep of the rectangle"""
         return {
             'id': self.id,
             'width': self.width,
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,15 +4,27 @@ from models.rectangle import Rectangle
 Defines a class Square
 """
 class Square(Rectangle):
+    """
+    Created a class square
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """initialization"""
+        """initialization
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """Return the print() and str() representation of a Square."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
+        """Defines the size of the square"""
         return self.width
     """sets property attribute"""
     @size.setter
@@ -20,6 +32,7 @@ class Square(Rectangle):
         self.width = self.height = value
 
     def to_dictionary(self):
+        """returns dict rep of the square"""
         return {
             'id': self.id,
             'size': self.size,
