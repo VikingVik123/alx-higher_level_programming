02ea8022cde59e1d70ec1e6990482921243a7295
VikingVik123 <vodimmegwa@gmail.com>
2024-03-11 21:29:35 -0700

    Fixed task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -10,5 +10,5 @@ const arg2 = parseInt(process.argv[3]);
 if (!isNaN(arg1) && !isNaN(arg2)) {
   console.log(add(arg1, arg2));
 } else {
-  console.log('Invalid input. Please provide two integers.');
+  console.log('NaN');
 }
