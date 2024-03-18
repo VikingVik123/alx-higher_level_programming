de0e41f8ea64e801382b040f0617ddff9fec31d0
VikingVik123 <vodimmegwa@gmail.com>
2024-03-18 05:18:23 -0700

    Added 3 & 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+            // If w or h is equal to 0 or not a positive integer, create an empty object
+            return {};
+        }
+        // Initialize the instance attributes width and height with the values of w and h
+        this.width = w;
+        this.height = h;
+    }
+
+    print() {
+        // Print the rectangle using the character 'X'
+        for (let i = 0; i < this.height; i++) {
+            console.log('X'.repeat(this.width));
+        }
+    }
+    rotate() {
+        this.width = this.height;
+        this.height = this.width;
+    }
+    double() {
+        this.width = 2 * this.width;
+        this.height = 2 * this.height;
+    }
+}
+
+module.exports = Rectangle;
