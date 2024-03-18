6bfd51077bd1fc7ad2178c4a172fb04f739def20
VikingVik123 <vodimmegwa@gmail.com>
2024-03-18 06:46:23 -0700

    Fixed task 5 & 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,14 +3,14 @@
 const Parent = require('./5-square.js');
 
 class Square extends Parent {
-    charPrint(c) {
-        if (c === undefined) {
-            c = 'X';
-        }
-        for (let i = 0; i < this.width; i++) {
-            console.log(c.repeat(this.width));
-        }
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 }
 
 module.exports = Square;
