9251d734866ddc6cf222c9b79088f891beacfa58
VikingVik123 <vodimmegwa@gmail.com>
2024-03-18 05:48:55 -0700

    Fixed task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,30 +1,34 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor(w, h) {
-        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-            // If w or h is equal to 0 or not a positive integer, create an empty object
-            return {};
-        }
-        // Initialize the instance attributes width and height with the values of w and h
-        this.width = w;
-        this.height = h;
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      // If w or h is equal to 0 or not a positive integer, create an empty object
+      return {};
     }
+    // Initialize the instance attributes width and height with the values of w and h
+    this.width = w;
+    this.height = h;
+  }
 
-    print() {
-        // Print the rectangle using the character 'X'
-        for (let i = 0; i < this.height; i++) {
-            console.log('X'.repeat(this.width));
-        }
+  print () {
+    // Print the rectangle using the character 'X'
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
-    rotate() {
-        this.width = this.height;
-        this.height = this.width;
-    }
-    double() {
-        this.width = 2 * this.width;
-        this.height = 2 * this.height;
-    }
+  }
+
+  rotate () {
+    // Swap width and height
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
 }
 
 module.exports = Rectangle;
