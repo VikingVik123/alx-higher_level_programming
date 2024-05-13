daf58a827276d8f8df919617b543095fff92b71b
VikingVik123 <vodimmegwa@gmail.com>
2024-05-13 14:16:34 -0700

    Added more files

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         charset="utf8")
 
     cursor = connection.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
     cursor.execute(query, (state_name,))
     rows = cursor.fetchall()
 
