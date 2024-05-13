36fa688350d003f18a7c18bf50e74dcbc17e496b
VikingVik123 <vodimmegwa@gmail.com>
2024-05-13 14:35:03 -0700

    Added more files

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" script that  lists all cities from the database hbtn_0e_4_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    connection = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("SELECT * FROM cities ORDER BY id ASC")
+        rows = cursor.fetchall()
+
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        print("Error:", e)
+    finally:
+        cursor.close()
+        connection.close()
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" script  takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    connection = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
+    cursor = connection.cursor()
+
+    try:
+        query = "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC"
+
+        cursor.execute(query, (state_name,))
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        print("Error:", e)
+    finally:
+        cursor.close()
+        connection.close()
+
