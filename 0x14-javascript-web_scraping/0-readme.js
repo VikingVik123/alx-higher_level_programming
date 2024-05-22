470f37f5050391a29ee947aeb1e402d439ef1d68
VikingVik123 <vodimmegwa@gmail.com>
2024-05-22 16:38:22 -0700

    Fixed task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', (err, content) =>
-{
-	if (err)
-		console.error(err);
-	else
-		console.log(content);
+fs.readFile(process.argv[2], 'utf8', (err, content) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(content);
+  }
 });
