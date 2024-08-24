262664fa400b0a2c9105278b57414703b93ca545
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-08-24 18:41:40 +0100

    Tried task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Write a function that prints a square with the character #
+"""
+
+
+def print_square(size):
+    """
+    function instantiation
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        print("#" * size)
