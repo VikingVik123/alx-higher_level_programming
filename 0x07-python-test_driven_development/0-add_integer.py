f8766176bf54849f03f6a0646ba3b6f4c91bec9a
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-08-24 13:32:58 +0100

    Tried task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Create addition function 
+Create addition function
 """
 
 
@@ -15,22 +15,8 @@ def add_integer(a, b=98):
         raise ValueError("b must be an integer")
     if a is float:
         a_value = int(a)
-        return(a_value)
+        return (a_value)
     if b is float:
         b_value = int(b)
-        return(b_value)
-    return(sum)
-
-if __name__ == "__main__":
-    print(add_integer(1, 2))
-    print(add_integer(100, -2))
-    print(add_integer(2))
-    print(add_integer(100.3, -2))
-    try:
-        print(add_integer(4, "School"))
-    except Exception as e:
-        print(e)
-    try:
-        print(add_integer(None))
-    except Exception as e:
-        print(e)
+        return (b_value)
+    return (sum)
